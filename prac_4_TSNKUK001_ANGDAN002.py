d8b86c143dd81b7e290bd81c86ce0e45b62d2bc6
Kukhokuhle <TSNKUK001@myuct.ac.za>
2018-09-18 19:57:10 +0200

    added the two callbacks for reset and frequency

--- prac_4_TSNKUK001_ANGDAN002.py
+++ prac_4_TSNKUK001_ANGDAN002.py
@@ -10,3 +10,23 @@ GPIO.setup(frequency, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(stop, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(display, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
+def clear():
+    _ = system('clear')
+
+
+# function definition: threaded callback
+def callback1(reset): #resets timer and clears console
+    global start_time
+    start_time = time.time()
+    
+    clear()
+
+
+def callback2(frequency):
+    global delay
+    if delay == 2:
+        delay = 0.5
+    elif delay == 1:
+        delay = 2
+    elif delay == 0.5:
+        delay = 1
