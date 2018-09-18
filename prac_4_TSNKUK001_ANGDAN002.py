90bf13add7955304be0d982bbbe93a35a189a870
Kukhokuhle <TSNKUK001@myuct.ac.za>
2018-09-18 13:25:04 +0200

    initialised pull-down resistors and defined clear() function

--- prac_4_TSNKUK001_ANGDAN002.py
+++ prac_4_TSNKUK001_ANGDAN002.py
@@ -21,3 +21,16 @@ reset = 4
 frequency = 17
 stop = 27
 display = 22
+
+#initialise pull down resistors for buttons
+GPIO.setup(reset, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(frequency, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(stop, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(display, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+# Define delay between readings
+delay = 0.5   #default delay is 500 ms = 0.5 s
+
+# define our clear function for clearing the console
+def clear():
+    _ = system('clear')
